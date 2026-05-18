b55331eeff7a9f246f6ee3c282c9b6f3083885fb
HarpreetSingh <180175553+cyberharpreet@users.noreply.github.com>
2026-05-18 13:05:12 +0530

    Update solid_waste.py

--- solid_waste.py
+++ solid_waste.py
@@ -1,33 +1,26 @@
-# Simple Solid Waste Prediction Project
+
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-# Read CSV file
 df = pd.read_csv("MinicipalGenerated.csv")
 
-# Take all year columns automatically
 years = df.columns[1:]
 
 X = []
 Y = []
 
-# Create input and output data
 for year in years:
-    X.append([int(year)])      # Input year
-    Y.append(df[year].sum())  # Total waste of all districts
+    X.append([int(year)])
+    Y.append(df[year].sum())
 
-# Create model
 model = LinearRegression()
 
-# Train model
 model.fit(X, Y)
 
-# Take year input from user
+predicted = model.predict(X)
+
 future_year = int(input("Enter year to predict waste: "))
 
-# Predict future waste
 prediction = model.predict([[future_year]])
-
-# Print result
 print("Predicted Solid Waste for", future_year, "=", prediction[0], "tons/year")
